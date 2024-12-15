b67b5be8c8d343f053018320ab6a86813fb09f31
xgui4 <145719319+xgui4ecole@users.noreply.github.com>
2024-12-15 02:24:18 -0500

    début de l'app

---
+++ X Launcher/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace X_Launcher.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ X Launcher/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace X_Launcher.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
